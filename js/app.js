e233a70d5cb85e530f1af3da2f10d597d362db44
Shariful Islam <sharifulmamun7@gmail.com>
2021-09-13 03:18:41 +0600

    error fixed

--- js/app.js
+++ js/app.js
@@ -9,8 +9,10 @@ loadProducts();
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
+
   for (const product of allProducts) {
-    const image = product.images;
+    const allProductsDiv = document.getElementById("all-products");
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -18,26 +20,28 @@ const showProducts = (products) => {
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <p><b>Category:</b> ${product.category}</p>
+      <p><b>Rating:</b>  ${product.rating.rate} (${product.rating.count})</p>
+      <h2><b>Price: $</b> ${product.price}</h2>
+      <button onclick="addToCart(${product.id},${product.price}),updateTotal()" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+
+      <button  id="details-btn" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="getProductDetails('${product.id}')">Details</button>
+      </div>
       `;
-    document.getElementById("all-products").appendChild(div);
+      allProductsDiv.appendChild(div);
   }
 };
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,12 +50,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -76,5 +80,31 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+
+//single product details
+const getProductDetails = id =>{
+  
+  const url =`https://fakestoreapi.com/products/${id}`
+  fetch(url)
+  .then(res =>res.json())
+  .then(data =>displayProductDetails(data))
+  
+}
+const displayProductDetails = singleProduct => {
+  //destructuring
+  const {title,image,category,description,price} = singleProduct;
+  const {rate,count} = singleProduct.rating;
+  document.getElementById('exampleModalLabel').innerText =title;
+  document.getElementById('modal-body').innerHTML =`
+  <img width="70%" src="${image}" alt="">
+    <p><b>Category</b>: ${category}</p>
+    <p><b>Description: </b> ${description.slice(0,200)}</p>
+    <p><b>Rating:</b>  ${rate} (${count})</p>
+    <h2>Price: $ ${price}</h2>
+  
+  
+  `
+} 
